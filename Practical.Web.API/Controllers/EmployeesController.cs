1a8d240c83e373bf39d0a216ec398e18b1190d8c
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-07 17:45:11 +0100

    feat(actions): Add actions to Employee controller for both GET method.

--- Practical.Web.API/Controllers/EmployeesController.cs
+++ Practical.Web.API/Controllers/EmployeesController.cs
@@ -3,9 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Practical.Web.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Returning from employee controller Get Method";
+        }
+
+        [HttpGet]
+        public string GetEmployee()
+        {
+            return "Returning from employee controller GetEmployee Method";
+        }
     }
 }
