4c82b84b0a335b3f1671add0999e4db7409f5c9e
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-04-12 18:28:47 +0100

    refact(controller): Refactore employee controller, implement dependency injection.

--- Practical.Web.API/Controllers/EmployeesController.cs
+++ Practical.Web.API/Controllers/EmployeesController.cs
@@ -1,24 +1,112 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Practical.Web.API.Models;
+using Practical.Web.API.Repositories;
 
 namespace Practical.Web.API.Controllers
 {
-    
+    [Route("api/[controller]")]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
-        [Route("api/[controller]/[action]")]
-        [HttpGet]
-        public string GetAllEmployees()
+
+        public readonly IEmployeeRepository _repository;
+
+        //Injects IEmployeeRepository to manage data operations.
+        public EmployeesController(IEmployeeRepository repository)
         {
-            return "Response from GetAllEmployees Method";
+            _repository = repository;
         }
 
-        [Route("api/[controller]/[action]/{Id}")]
         [HttpGet]
-        public string GetEmployeeById(int Id)
+        public ActionResult<IEnumerable<Employee>> GetAllEmployees()
         {
-            return $"Reponse from GetEmployeeById Method Id: {Id}";
+            var employees = _repository.GetAll();
+
+            return Ok(employees);
+
+        }
+
+        [HttpGet("{Id}")]
+        public ActionResult<Employee> GetEmployeeById(int Id)
+        {
+            
+            var employee = _repository.GetById(Id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> CreateEmployee([FromBody] Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _repository.Add(employee);
+
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult updateEmployee(int id, [FromBody] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest("Employ ID Mismatch.");
+            }
+
+            if (!_repository.Exists(id))
+            {
+                return NotFound();
+            }
+
+            _repository.Update(employee);
+
+            return NoContent();
+
+        }
+
+        // Partially updates an existing employee (PATCH api/employee/{id}).
+        [HttpPatch("{id}")]
+        public IActionResult patchEmployee(int id, [FromBody] Employee employee)
+        {
+            var existingEmployee = _repository.GetById(id);
+
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+
+            // For simplicity, updating all fields. In real scenarios, use JSON Patch.
+            existingEmployee.Name = employee.Name ?? employee.Name;
+            existingEmployee.Position = employee.Position ?? employee.Position;
+            existingEmployee.Age = employee.Age != 0 ? employee.Age : existingEmployee.Age;
+            existingEmployee.Email = employee.Email ?? employee.Email;
+
+            _repository.Update(existingEmployee);
+
+            return NoContent();
+        }
+
+        // Deletes an employee (DELETE api/employee/{id}).
+        [HttpDelete("{id}")]
+        public IActionResult deleteEmployee(int id)
+        {
+            if (!_repository.Exists(id))
+            {
+                return NotFound();
+            }
+            _repository.Delete(id);
+
+            return NoContent();
         }
     }
 }
--- Practical.Web.API/Program.cs
+++ Practical.Web.API/Program.cs
@@ -1,3 +1,5 @@
+using Practical.Web.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register the EmployeeRepository for Dependency Injection
+builder.Services.AddSingleton<IEmployeeRepository, EmployeeRespository>();
 
 var app = builder.Build();
 
